a737325e5b3ad003e5f4c5c1b7d3cc86bfaf6f58
dj-Andres <andres96jimenez@gmail.com>
2021-11-30 10:25:34 -0500

    fix the file the env

--- config/config.php
+++ config/config.php
@@ -1,8 +1,7 @@
 <?php
 
- define("APP_SERVER","localhost");
- define("USER","root");
- define("PASSWORD","");
- define("DATABASE","farmacia");
+ define("APP_SERVER","bef3gjnj0zqo4qxcobay-mysql.services.clever-cloud.com");
+ define("USER","uflgxhwn7dxg9y1l");
+ define("PASSWORD","aCKaMDiZaEh4wqEMk0zE");
+ define("DATABASE","bef3gjnj0zqo4qxcobay");
  define("PORT","3306");
-
